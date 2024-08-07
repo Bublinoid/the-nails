8081946200befc957781db8fe9af4d62133bda4b
Hleb <gleb.qb@gmail.com>
2024-08-07 15:13:29 +0200

    init

---
+++ src/main/java/ru/bublinoid/thenails/TheNailsApplication.java
@@ -0,0 +1,13 @@
+package ru.bublinoid.thenails;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TheNailsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TheNailsApplication.class, args);
+	}
+
+}
---
+++ src/test/java/ru/bublinoid/thenails/TheNailsApplicationTests.java
@@ -0,0 +1,13 @@
+package ru.bublinoid.thenails;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TheNailsApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
